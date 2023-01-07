131de113d77d5b36842033e1270f217b28e3903f
Tarek Hosen <58102236+tarekhosen1111@users.noreply.github.com>
2023-01-07 10:52:58 +0600

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    int x;
+    //scanf("%2d%4d",&a,&b);
+    //printf("%d %d",a,b);
+    //scanf("%d %d",&a,&x);
+   // printf("%d %d",a,x);
+   scanf("%3d %4d %3d",&a,&b,&c);
+   printf("%d %d %d",a,b,c);
+}
